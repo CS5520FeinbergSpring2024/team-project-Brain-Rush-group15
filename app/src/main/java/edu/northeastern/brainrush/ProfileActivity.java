583efc19bd42d8b77d129122f78af5f57bbe0854
Zhuofeng Wang <zw2922@nyu.edu>
2024-04-01 16:24:03 -0700

    Use the user photo library to set the profile picture and upload it to the firebase storage

--- app/src/main/java/edu/northeastern/brainrush/ProfileActivity.java
+++ app/src/main/java/edu/northeastern/brainrush/ProfileActivity.java
@@ -1,14 +1,20 @@
 package edu.northeastern.brainrush;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -18,11 +24,15 @@ import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 
 public class ProfileActivity extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_CODE = 1;
+    private static final int PICK_IMAGE_REQUEST = 2;
+
     private ImageView profilePicture;
 
     private TextView name;
     private TextView score;
     private TextView level;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -39,6 +49,63 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     public void setProfilePicture(View view){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_MEDIA_IMAGES}, PERMISSION_REQUEST_CODE);
+        } else {
+            // Permission has already been granted
+            pickImage();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                pickImage();
+            } else {
+                // Permission denied
+            }
+        }
+    }
+
+    private void pickImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri selectedImageUri = data.getData();
+            // Handle the selected image URI (e.g., display it in an ImageView)
+            Log.v("picture selected", String.valueOf(selectedImageUri));
+            profilePicture.setImageURI(selectedImageUri);
+            storeProfilePicture(selectedImageUri);
+        }
+    }
+
+    public void storeProfilePicture(Uri uri){
+        // Get an instance of FirebaseStorage
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        // Create a storage reference
+        StorageReference storageRef = storage.getReference();
+        // Create a reference to "profilePictures/userId.jpg"
+        StorageReference profilePicRef = storageRef.child("profilePictures/nick.jpg");
+
+        profilePicRef.putFile(uri)
+                .addOnSuccessListener(taskSnapshot -> {
+                    // Get the download URL
+                    profilePicRef.getDownloadUrl().addOnSuccessListener(downloadUri -> {
+                        // Use this download URL for the user's profile picture
+                    });
+                })
+                .addOnFailureListener(exception -> {
+                    // Handle unsuccessful uploads
+                });
 
     }
 
@@ -54,7 +121,6 @@ public class ProfileActivity extends AppCompatActivity {
             public void onSuccess(Uri uri) {
                 // Got the download URL for 'users/me/profile.png'
                 Picasso.get().load(uri).into(profilePicture);
-
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
